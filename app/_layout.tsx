74759bdf7378107e20f7a72250742675aca674f5
TheSaiTeja <iamsaiteja2411@gmail.com>
2025-12-01 19:07:37 +0530

    chore: update package dependencies and add new font family to Tailwind config
    - Changed expo-font version from ~14.0.9 to ^14.0.9 in package.json
    - Added react-native-heroicons dependency with version ^4.0.0
    - Added depcheck as a devDependency with version ^1.4.7
    - Extended Tailwind CSS configuration to include a new font family 'monoton'

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,28 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 import "./globals.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [loaded] = useFonts({
+    Monoton: require("../assets/fonts/Monoton-Regular.ttf"),
+  });
+
+  // Keep splash screen visible until fonts are loaded
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null; // or leave splash screen up
+  }
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        monoton: ["Monoton_400Regular"],
+      },
+    },
   },
   plugins: [],
 };
