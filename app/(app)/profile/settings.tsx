4f8087ff6e76ce1c1eb8bdeb2fd9140f3ab5ba39
TheSaiTeja <iamsaiteja2411@gmail.com>
2025-12-09 13:51:42 +0530

    feat(auth): implement logout feature

--- app/(app)/profile/settings.tsx
+++ app/(app)/profile/settings.tsx
@@ -1,10 +1,26 @@
+import { useAuth } from "@/stores/authStore";
+import { AntDesign } from "@expo/vector-icons";
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Settings() {
+  const logout = useAuth((state) => state.logout);
+
+  const handleLogout = () => {
+    logout();
+  };
+
   return (
-    <View className="h-full bg-white dark:bg-black">
-      <Text>Settings</Text>
+    <View className="flex items-center  h-full bg-white dark:bg-black p-4">
+      <TouchableOpacity
+        onPress={handleLogout}
+        className="flex flex-row items-center justify-start w-full gap-4 rounded-2xl border border-neutral-200/60 dark:border-neutral-800 bg-white dark:bg-neutral-900 p-4 shadow-sm"
+      >
+        <AntDesign name="logout" size={24} color="red" />
+        <Text className="text-black dark:text-white font-semibold text-xl">
+          Logout{" "}
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
